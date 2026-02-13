55d161858d607210f319e81092c8ff456797f445
uroa375-blip <uroa375@gmail.com>
2026-02-13 18:01:51 +0900

    Fix case-sensitivity build error in Setting.jsx

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/setting.css';
+import '../styles/Setting.css';
 
 const Setting = () => {
     const navigate = useNavigate();
